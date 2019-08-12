c33e3a0bc04fda9356588a1eebd5cb624d9d64d5
Matthew Healy <mhealy@cboe.com>
2019-08-12 09:18:42 -0500

    Configure dashboard

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,9 +7,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" default)))
+ '(global-whitespace-mode nil)
  '(package-selected-packages
    (quote
-    (cmake-font-lock font-lock+ use-package projectile org multiple-cursors helm god-mode doom-modeline company atom-one-dark-theme))))
+    (rjsx-mode multi-web-mode web-mode mmm-mode treemacs-magit treemacs-icons-dired treemacs-projectile magit rainbow-delimiters atom-dark-theme cmake-font-lock font-lock+ use-package projectile org multiple-cursors helm god-mode doom-modeline company atom-one-dark-theme)))
+ '(projectile-completion-system (quote ivy))
+ '(projectile-enable-caching t)
+ '(projectile-indexing-method (quote alien)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
